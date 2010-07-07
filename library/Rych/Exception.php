fb049472730eac253d4adada72d5c5bcd204f692
Ryan Chouinard <rchouinard@gmail.com>
2010-07-07 00:45:04 +0000

    Created library base exception.

---
+++ library/Rych/Exception.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Ryan Chouinard's Components
+ *
+ * @category    Rych
+ * @package     Rych
+ * @author      Ryan Chouinard <rchouinard@gmail.com>
+ * @copyright   Copyright (c) 2009 Ryan Chouinard ({@link http://ryanchouinard.com})
+ * @license     New BSD License
+ * @version     $Id$
+ */
+
+/**
+ * @uses        Exception
+ * @category    Rych
+ * @package     Rych
+ * @author      Ryan Chouinard <rchouinard@gmail.com>
+ * @license     New BSD License
+ */
+class Rych_Exception extends Exception
+{
+}
